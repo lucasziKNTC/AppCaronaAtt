af5fb385407135c02d93cfeaa15359f9d27e39ab
Lucas Gabriel Oliveira da Silva <040161@campus1.uniaraxa.edu.br>
2021-12-02 21:42:12 -0300

    Repository

---
+++ PROJETO01.Dados/EntityFramework/Contexto.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using PROJETO01.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+
+namespace PROJETO01.Dados.EntityFramework
+{
+    public class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var conn = @"Data source = 201.62.57.93; 
+                                     Database = dbLAB2_2020; 
+                                     User ID = visualstudio; 
+                                     Password = visualstudio";
+            optionsBuilder.UseSqlServer(conn);
+        }
+
+        //Nome das Classes que representarão o Banco de Dados
+        public DbSet<Estado> Estado { get; set; }
+
+        public DbSet<Cidade> Cidade { get; set; }
+
+        public DbSet<CadastroDePessoa> CadastroDePessoa { get; set; }
+
+        public DbSet<SolicitarCarona> SolicitarCarona { get; set; }
+
+        //Definição das características das classes com as tabelas
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Estado>()
+                .ToTable("Estado")
+                .HasKey("UF");
+
+            modelBuilder.Entity<Estado>()
+                .Property("UF")
+                .HasColumnName("Sigla_Est")
+                .HasColumnType("char(2)")
+                .IsRequired();
+
+            modelBuilder.Entity<Estado>()
+                .Property("Nome")
+                .HasColumnName("Nome_Est")
+                .HasColumnType("varchar(100)")
+                .IsRequired();
+
+            modelBuilder.Entity<Cidade>()
+             .ToTable("Cidade")
+             .HasKey("CidadeId");
+
+
+            modelBuilder.Entity<Cidade>()
+                .Property("CidadeId")
+                .HasColumnName("CidadeId")
+                .HasColumnType("int")
+                .IsRequired();
+
+            modelBuilder.Entity<Cidade>()
+               .Property("Nome")
+               .HasColumnName("Cidade")
+               .HasColumnType("varchar(100)")
+               .IsRequired();
+
+            modelBuilder.Entity<Cidade>()
+              .Property("UF")
+              .HasColumnName("UF")
+              .HasColumnType("char(2)")
+              .IsRequired();
+
+
+
+
+
+
+
+
+
+
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+
+}
---
+++ PROJETO01.Modelos/SolicitarCarona.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PROJETO01.Modelos
+{
+   public  class SolicitarCarona
+    {
+        public int MotoristaId { get; set; }
+
+        public int PessoaId { get; set; }
+
+        public string EstadoID { get; set; }
+
+        public string SolicitarID { get; set; }
+
+        public string Cidade { get; set; }
+
+        public string Avaliacao { get; set; }
+    }
+}
